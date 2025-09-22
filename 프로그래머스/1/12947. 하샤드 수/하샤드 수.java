3f9ec2c53f6e9b06d22493ce11d0aa57ea8b163d
Jeon Wooseon <jeonwooseon@naver.com>
2025-09-22 10:16:18 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 95.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int temp = x;
+        
+        while(temp>0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        return x % sum == 0;
+    }
+}
