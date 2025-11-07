4164bd4c382ce1c7d6820b31d6f5d08567a8d2db
Jeon Wooseon <jeonwooseon@naver.com>
2025-11-07 10:55:57 +0900

    [level 1] Title: 소수 만들기, Time: 4.96 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int solution(int[] nums) {
+        int count = 0;
+
+        // 3중 for문으로 조합 구하기 (i < j < k)
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count;
+    }
+
+    // 소수 판별 함수
+    private boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
